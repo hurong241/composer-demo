d299a173d59100d557afe9fd703e1d87bbd33be7
胡熔 <3136875651@qq.com>
2023-02-12 21:42:35 +0800

    hyperf取服务端ip

--- src/JsonRpc/IpInterface.php
+++ src/JsonRpc/IpInterface.php
@@ -6,7 +6,9 @@
 
 namespace Hurong\ComposerDemo\JsonRpc;
 
+use Hyperf\HttpServer\Contract\RequestInterface;
+
 interface IpInterface
 {
-    public function ip(): string;
+    public function ip(RequestInterface $request): string;
 }
